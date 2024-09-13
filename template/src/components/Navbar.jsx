27f853554dda6c1fb19d164b9f8d4ccfc8a9ce11
cem <cemyilmaztech@gmail.com>
2024-09-14 00:29:32 +0300

    Navbar link

--- template/src/components/Navbar.jsx
+++ template/src/components/Navbar.jsx
@@ -1,26 +1,26 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 const Navbar = () => {
   return (
     <nav class="navbar navbar-expand-lg bg-secondary">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand text-white" href="#">Shoppinng Card</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
+      <ul class="navbar-nav ">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
+          <Link className='nav-link' to="/">Home</Link>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Features</a>
+          <Link className='nav-link' to="/New Product">New Product</Link>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Pricing</a>
+          <Link className='nav-link' to="/Product List">Product List</Link>
         </li>
         <li class="nav-item">
-          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+          <Link className='nav-link' to="/About">About</Link>
         </li>
       </ul>
     </div>
