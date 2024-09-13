a82ede04e4b3df5640850bec898efd32040fb75f
cem <cemyilmaztech@gmail.com>
2024-09-14 00:00:24 +0300

    first commit

---
+++ template/src/App.js
@@ -0,0 +1,28 @@
+
+// import Main from "./pages/Main";
+// import NewProduct from "./pages/NewProduct";
+// import ProductList from "./pages/ProductList";
+// import About from "./pages/About";
+// import Navbar from "./components/Navbar";
+
+import AppRouter from "./router/AppRouter";
+
+function App() {
+
+  
+
+  return (
+    <div className="bg-light app">
+       {/* <AppRouter/> */}
+      {/* <Navbar/>
+      <Main/>
+      <About/>
+      <NewProduct/>
+      <ProductList/> */}
+    
+    </div>
+     
+  );
+}
+
+export default App;
---
+++ template/src/router/AppRouter.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import { BrowserRouter } from 'react-router-dom'
+import { Route,Routes } from 'react-router-dom'
+import About from '../pages/About'
+import Main from '../pages/Main'
+import ProductForm from '../components/ProductForm'
+import ProductCard from '../components/ProductCard'
+import NotFound from '../components/NotFound'
+import NewProduct from '../pages/NewProduct'
+
+
+const AppRouter = () => {
+  return (
+  <BrowserRouter>
+<Navbar/>
+  <Routes>
+
+<Route path="/About" element={<About/>}/>
+<Route path="/" element={<Main/>}/>
+<Route path="/New product" element={<NewProduct/>}/>
+<Route path="/Product List" element={<ProductCard/>}/>
+<Route path="/UpdateProduct" element={<ProductForm/>}/>
+<Route path="*" element={<NotFound/>}/>
+
+
+
+
+
+
+
+  </Routes>
+  
+  
+  
+  </BrowserRouter>
+  )
+}
+
+export default AppRouter
